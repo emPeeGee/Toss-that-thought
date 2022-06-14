f4c5d06bf9691ba2ffadf0fbb762b9612768c53c
Popovici Mihail <mihail.popovici@happeo.com>
2022-06-14 18:27:48 +0300

    [frontend] feat: Init to work on Thought feature

---
+++ frontend/src/components/InputsAndButtons/Select/Select.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyledSelect } from './Select.styles';
+
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+export function Select({ label }: { label: string }) {
+  return <StyledSelect />;
+}
--- frontend/src/features/thoughts/Thought.tsx
+++ frontend/src/features/thoughts/Thought.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Button } from '@mantine/core';
+import { Container, Textarea, Input, Select } from '@mantine/core';
 
 export function Thought() {
   return (
-    <div>
-      <h1>Thought</h1>
-      <Button>Hey</Button>
-    </div>
+    <Container size="md">
+      <h2>Paste a password, secret message or private link below.</h2>
+      <h3>Keep sensitive info out of your email and chat logs.</h3>
+
+      <Textarea
+        aria-label="Paste a password, secret message or private link below"
+        placeholder="Autosize with no rows limit"
+        label="Autosize with no rows limit"
+        autosize
+        minRows={10}
+      />
+
+      <Input placeholder="Your email" radius="md" size="md" />
+      <Select
+        label="Your favorite framework/library"
+        placeholder="Pick one"
+        data={[
+          { value: '7', label: '7 days' },
+          { value: '3', label: '3 days' },
+          { value: '1', label: '1 days' }
+        ]}
+      />
+    </Container>
   );
 }
