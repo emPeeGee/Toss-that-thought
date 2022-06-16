5d1a5f3a55a6c38953bab24f40c335955745a2b8
Popovici Mihail <mihail.popovici@happeo.com>
2022-06-16 18:12:40 +0300

    [backend] feat: Add logger when request and cors policy

--- backend/pkg/handler/handler.go
+++ backend/pkg/handler/handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"github.com/emPeeee/ttt/pkg/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"time"
 )
 
 type Handler struct {
@@ -17,10 +19,20 @@ func NewHandler(services *service.Service, validator *validator.Validate) *Handl
 
 func (h *Handler) InitializeRoutes() *gin.Engine {
 	router := gin.New()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.Use(gin.Logger())
 
 	api := router.Group("/api")
 	{
-		api.POST("/create", h.Create)
+		api.POST("/create", h.Create) // Good to rename endpoint
 		api.GET("/metadata/:id", h.RetrieveMetadata)
 		api.GET("/thought/:id", h.ThoughtExists)
 		api.POST("/thought/:id", h.RetrieveThought)
